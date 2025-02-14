19099e9cd7d872c210255cfe787093c204fbf234
ana <ana@192.168.1.5>
2025-02-14 15:32:56 +0100

    fake gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+         PROFILE_TITLE = [
+          'Senior Ruby on rails developer',
+          'full stack developer',
+           'junior ruby on rails developer',
+           'senior full stack developer',
+           'senior javascript developer',
+           'senior frontend developer'
+].freeze
+
+def name
+  "#{first_name} #{last_name}".strip
+end
 end
